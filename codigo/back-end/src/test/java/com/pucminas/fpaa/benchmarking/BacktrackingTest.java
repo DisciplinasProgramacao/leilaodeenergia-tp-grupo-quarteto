a62ea0e2141619c0cd51931f2184bd5ac97c212c
Izabela Cecilia <icsbarbosa@sga.pucminas.br>
2024-06-11 20:09:41 -0300

    feat: refac

--- codigo/back-end/src/test/java/com/pucminas/fpaa/benchmarking/BacktrackingTest.java
+++ codigo/back-end/src/test/java/com/pucminas/fpaa/benchmarking/BacktrackingTest.java
@@ -1,13 +1,10 @@
 package com.pucminas.fpaa.benchmarking;
 
-
 import com.pucminas.fpaa.dtos.ResultadoDTO;
 import com.pucminas.fpaa.entity.EmpresaInteressada;
 import com.pucminas.fpaa.entity.EmpresaVendedora;
 import com.pucminas.fpaa.model.LeilaoSolverBacktrack;
 import com.pucminas.fpaa.repositories.EmpresaInteressadaRepository;
-import com.pucminas.fpaa.repositories.EmpresaVendedoraRepository;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +15,7 @@ import org.springframework.test.context.jdbc.Sql;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
+import java.util.Random;
 
 @ActiveProfiles("test")
 @SpringBootTest
@@ -26,44 +24,55 @@ import java.util.concurrent.TimeUnit;
 public class BacktrackingTest {
 
     @Autowired
-    private  LeilaoSolverBacktrack backtrackService;
+    private LeilaoSolverBacktrack backtrackService;
+
     @Autowired
-    private  EmpresaInteressadaRepository empresaInteressadaRepository;
-
+    private EmpresaInteressadaRepository empresaInteressadaRepository;
     private static final int TEMPO_LIMITE_SEGUNDOS = 30;
-
+    private static final Random RANDOM = new Random(42);
 
     @Test
     void testBacktrackingPerformance() {
-        int maxTamanho=1;
+        int tamanhoAtual = 10;
         boolean limiteAlcancado = false;
-        List<Double> temposExecucao = new ArrayList<>();
 
         while (!limiteAlcancado) {
+            List<Double> temposExecucao = new ArrayList<>();
 
-            ResultadoDTO resultadoDTO = executarTeste(3L);
-            long duracaoSegundos = TimeUnit.NANOSECONDS.toSeconds(resultadoDTO.getDuracao());
-            temposExecucao.add((double) duracaoSegundos);
+            for (int i = 0; i < 10; i++) {
+                limparEmpresasInteressadas();
+                if (i == 0) adicionarEmpresasInteressadas(0);
+                else adicionarEmpresasInteressadas(tamanhoAtual);
 
-            if(duracaoSegundos > TEMPO_LIMITE_SEGUNDOS) {
-                limiteAlcancado = true;
-                System.out.println("Tempo de limite estourado na execução: "+ maxTamanho + " com tamanho: "+ (maxTamanho + 10));
-                break;
+                ResultadoDTO resultado = executarTeste(3L);
+                long duracaoSegundos = TimeUnit.NANOSECONDS.toSeconds(resultado.getDuracao());
+                temposExecucao.add((double) duracaoSegundos);
+
+                System.out.println("Execução " + (i + 1) + " com tamanho " + tamanhoAtual + ": " + duracaoSegundos + " segundos");
             }
-            adicionarEmpresasInteressadas(maxTamanho++);
-        }
-        if(limiteAlcancado) {
-            double mediaDuracao = temposExecucao.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
-            System.out.println("Tamanho: " + (maxTamanho + 10) + " - Média de execução: " + mediaDuracao + " segundos");
 
+            double mediaDuracao = temposExecucao.stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+            System.out.println("Tamanho: " + tamanhoAtual + " - Média de execução: " + mediaDuracao + " segundos");
+
+            if (mediaDuracao > TEMPO_LIMITE_SEGUNDOS) {
+                System.out.println("Tempo limite estourado na média com tamanho: " + tamanhoAtual);
+                limiteAlcancado = true;
+            } else {
+                tamanhoAtual++;
+            }
         }
     }
 
+    private void limparEmpresasInteressadas() {
+        empresaInteressadaRepository.deleteAll();
+    }
+
     private void adicionarEmpresasInteressadas(int quantidade) {
+        for (int i = 1; i <= quantidade; i++) {
             EmpresaInteressada empresaInteressada = EmpresaInteressada.builder()
-                    .nome("Empresa L" + quantidade)
-                    .quantRequerida((100 % quantidade) * 10)
-                    .valor((double) (100 + quantidade * 10))
+                    .nome("Empresa L" + i)
+                    .quantRequerida((RANDOM.nextInt(60) + 1) * 10)
+                    .valor((double) (100 + RANDOM.nextInt(500)))
                     .empresaVendedora(
                             EmpresaVendedora.builder()
                                     .id(3L)
@@ -72,7 +81,9 @@ public class BacktrackingTest {
                                     .quant_disponivel(1000)
                                     .build()
                     ).build();
+            System.out.println(empresaInteressada.getQuantRequerida()+"\n");
             empresaInteressadaRepository.save(empresaInteressada);
+        }
     }
 
     private ResultadoDTO executarTeste(Long empresaId) {
@@ -88,4 +99,3 @@ public class BacktrackingTest {
         return resultado;
     }
 }
-
