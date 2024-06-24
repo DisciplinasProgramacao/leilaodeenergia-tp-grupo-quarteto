85a83a47843bacd1fe4a2d5f4929cbcae3c5441c
Rodrigo Mendes <rodrigomendespeixoto009@gmail.com>
2024-06-24 01:32:32 -0300

    Alteração no divisão e conquista para ficar semelhante ao executado em sala

--- codigo/back-end/src/main/java/com/pucminas/fpaa/model/LeilaoSolverDivisaoEConquista.java
+++ codigo/back-end/src/main/java/com/pucminas/fpaa/model/LeilaoSolverDivisaoEConquista.java
@@ -10,13 +10,12 @@ import com.pucminas.fpaa.interfaces.LeilaoSolverDivisaoEConquistaI;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 @Service
 public class LeilaoSolverDivisaoEConquista implements LeilaoSolverDivisaoEConquistaI {
     private final EmpresaVendedoraRepository _empresaVendedoraRepository;
     private final EmpresaInteressadaRepository _empresaInteressadaRepository;
-    private List<EmpresaInteressada> empresasSelecionadas = new ArrayList<>();
-
     public LeilaoSolverDivisaoEConquista(EmpresaVendedoraRepository _empresaVendedoraRepository, EmpresaInteressadaRepository _empresaInteressadaRepository, List<EmpresaInteressada> empresasSelecionadas) {
         this._empresaVendedoraRepository = _empresaVendedoraRepository;
         this._empresaInteressadaRepository = _empresaInteressadaRepository;
@@ -28,7 +27,7 @@ public class LeilaoSolverDivisaoEConquista implements LeilaoSolverDivisaoEConqui
         if (empresaVendedora != null) {
             resultadoDTO.iniciarContagem();
             List<EmpresaInteressada> empresasInteressadas = _empresaInteressadaRepository.findByEmpresaVendedoraId(idEmpresa);
-            resultadoDTO.setMelhorSelecao(resolverLeilaoDivisaoEConquista(empresasInteressadas, empresaVendedora.getQuant_disponivel() - empresaVendedora.getQuant_vendida(), empresasInteressadas.size() - 1));
+            resultadoDTO.setMelhorSelecao(resolverLeilaoDivisaoEConquista(empresasInteressadas, empresaVendedora.getQuant_disponivel() - empresaVendedora.getQuant_vendida()));
             resultadoDTO.setAlgoritmoUtilizado(AlgoritmoEnum.DIVISAOECONQUISTA);
             resultadoDTO.setQuantidadeVendida(resultadoDTO.getMelhorSelecao().stream().mapToInt(EmpresaInteressada::getQuantRequerida).sum());
             resultadoDTO.setMelhorLucro(resultadoDTO.getMelhorSelecao().stream().mapToDouble(EmpresaInteressada::getValor).sum());
@@ -38,26 +37,48 @@ public class LeilaoSolverDivisaoEConquista implements LeilaoSolverDivisaoEConqui
         }
         return resultadoDTO;
     }
-    private  List<EmpresaInteressada> resolverLeilaoDivisaoEConquista(List<EmpresaInteressada> empresas, int energiaDisponivel, int index) {
-        if (index < 0 || energiaDisponivel <= 0) {
-            return new ArrayList<>();
+
+    private List<EmpresaInteressada> resolverLeilaoDivisaoEConquista(List<EmpresaInteressada> empresasInteressadas, int quantidadeEnergiaDisponivel){
+        List<EmpresaInteressada> empresasSelecionadas = new ArrayList<>();
+        int valorInicial = empresasInteressadas.stream().min(Comparator.comparing(EmpresaInteressada::getQuantRequerida)).orElse(null).getQuantRequerida();
+        if(quantidadeEnergiaDisponivel/2 <= valorInicial){
+            EmpresaInteressada empresaSelecionada =  empresasInteressadas.stream().filter(e -> e.getQuantRequerida() <= quantidadeEnergiaDisponivel)
+                    .max(Comparator.comparing(EmpresaInteressada::getValor)).orElse(null);
+            if(empresaSelecionada != null){
+                empresasInteressadas.remove(empresaSelecionada);
+                empresasSelecionadas.add(empresaSelecionada);
+            }
+            return empresasSelecionadas;
         }
-        if (empresas.get(index).getQuantRequerida() > energiaDisponivel) {
-            return resolverLeilaoDivisaoEConquista(empresas, energiaDisponivel, index - 1);
-        } else {
-            List<EmpresaInteressada> incluirEmpresa = new ArrayList<>();
-            incluirEmpresa.addAll(resolverLeilaoDivisaoEConquista(empresas, energiaDisponivel - empresas.get(index).getQuantRequerida(), index - 1));
-            incluirEmpresa.add(empresas.get(index));
+            List<EmpresaInteressada> direita = resolverLeilaoDivisaoEConquista(empresasInteressadas, quantidadeEnergiaDisponivel/2);
+            List<EmpresaInteressada> esquerda = resolverLeilaoDivisaoEConquista(empresasInteressadas, quantidadeEnergiaDisponivel/2);
+        return compararListas(empresasInteressadas, direita, esquerda, quantidadeEnergiaDisponivel);
+    }
 
-            List<EmpresaInteressada> naoIncluirEmpresa = resolverLeilaoDivisaoEConquista(empresas, energiaDisponivel, index - 1);
-            double valorIncluir = incluirEmpresa.stream().mapToDouble(EmpresaInteressada::getValor).sum();
-            double valorNaoIncluir = naoIncluirEmpresa.stream().mapToDouble(EmpresaInteressada::getValor).sum();
-
-            if (valorIncluir > valorNaoIncluir) {
-                return incluirEmpresa;
-            } else {
-                return naoIncluirEmpresa;
+    private List<EmpresaInteressada> compararListas(List<EmpresaInteressada> empresasInteressadas, List<EmpresaInteressada> direita, List<EmpresaInteressada> esquerda, int quantidadeEnergiaDisponivel) {
+        List<EmpresaInteressada> empresasSelecionadas = new ArrayList<>(direita);
+        empresasSelecionadas.addAll(esquerda);
+        int valorTotalInserido = (int)empresasSelecionadas.stream().mapToDouble(EmpresaInteressada::getValor).sum();
+        int quantidadeTotalInserido = (int)empresasSelecionadas.stream().mapToDouble(EmpresaInteressada::getQuantRequerida).sum();
+        if(quantidadeEnergiaDisponivel - empresasSelecionadas.stream().mapToInt(EmpresaInteressada::getQuantRequerida).sum() > 0){
+            //Adiciona Empresa caso tenha energia disponivel
+            EmpresaInteressada empresaSelecionada =  empresasInteressadas.stream()
+                    .filter(e -> e.getQuantRequerida() <= quantidadeEnergiaDisponivel- quantidadeTotalInserido).max(Comparator.comparing(EmpresaInteressada::getValor)).orElse(null);
+            if(empresaSelecionada != null){
+                empresasInteressadas.remove(empresaSelecionada);
+                empresasSelecionadas.add(empresaSelecionada);
+                valorTotalInserido = (int)empresasSelecionadas.stream().mapToDouble(EmpresaInteressada::getValor).sum();
             }
         }
+        int finalValorTotal = valorTotalInserido;
+        EmpresaInteressada empresaSelecionadaTroca =  empresasInteressadas.stream()
+                .filter(e -> e.getQuantRequerida() <= quantidadeEnergiaDisponivel && e.getValor() > finalValorTotal).findFirst().orElse(null);
+        if(empresaSelecionadaTroca != null){
+            empresasInteressadas.addAll(empresasSelecionadas);
+            empresasInteressadas.remove(empresaSelecionadaTroca);
+            empresasSelecionadas.clear();
+            empresasSelecionadas.add(empresaSelecionadaTroca);
+        }
+        return empresasSelecionadas;
     }
 }
